8c2d5e22123a709765e3d6ed50bba4ccc428c738
Foxkid <alserkol90@gmail.com>
2026-06-06 02:29:21 +0300

    solving task № 1

---
+++ src/test/java/org/netology/exceptions/ShopRepositoryTest.java
@@ -0,0 +1,45 @@
+package org.netology.exceptions;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.netlogy.exceptions.NotFoundException;
+import org.netlogy.exceptions.Product;
+import org.netlogy.exceptions.ShopRepository;
+
+public class ShopRepositoryTest {
+
+    @Test
+    public void shouldRemoveProductSuccess() {
+        ShopRepository shopRepo = new ShopRepository();
+        Product product1 = new Product(1, "Баскетбольный мяч", 500);
+        Product product2 = new Product(2, "Футбольный мяч", 800);
+        Product product3 = new Product(1, "Теннисный мяч", 300);
+
+        shopRepo.add(product1);
+        shopRepo.add(product2);
+        shopRepo.add(product3);
+
+        shopRepo.removeById(2);
+
+        Product[] expected = {product1, product3};
+        Product[] actual = shopRepo.findAll();
+        Assertions.assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldGenerateException() {
+        ShopRepository shopRepo = new ShopRepository();
+        Product product1 = new Product(1, "Баскетбольный мяч", 500);
+        Product product2 = new Product(2, "Футбольный мяч", 800);
+        Product product3 = new Product(1, "Теннисный мяч", 300);
+
+        shopRepo.add(product1);
+        shopRepo.add(product2);
+        shopRepo.add(product3);
+
+        Assertions.assertThrows(NotFoundException.class, () -> {shopRepo.removeById(15);
+        });
+
+
+    }
+}
